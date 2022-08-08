e5cb78817482c69c5d32a7736228017da37c481f
OlenaHras <alena.hras@gmail.com>
2022-08-08 20:02:02 +0300

    fixed 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,7 +4,7 @@ const form = document.querySelector('.feedback-form');
 const email = document.querySelector('.feedback-form input');
 const message = document.querySelector('.feedback-form textarea');
 
-const formData = {};
+let formData = {};
 
 form.addEventListener('submit', onFormSybmit);
 form.addEventListener('input', throttle(onFormData, 500));
@@ -14,7 +14,7 @@ getSavedMessage();
 function onFormData (event) {  
   formData[event.target.name] = event.target.value;
   const formStringified = JSON.stringify(formData);  
-  localStorage.setItem("feedback-form-state", formStringified); 
+  localStorage.setItem("feedback-form-state", formStringified);
 };
 
 function onFormSybmit(event) {
@@ -33,6 +33,7 @@ function getSavedMessage() {
   const savedMessage = localStorage.getItem('feedback-form-state');
   if (savedMessage) {
     const parced = JSON.parse(savedMessage);
+    formData = parced;
     email.value = parced.email ? parced.email : '';
     message.value = parced.message ? parced.message : '';
   };
